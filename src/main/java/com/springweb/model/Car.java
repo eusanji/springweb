e00a7374ad8dc8c4e10af35e4c9af5da87e85ca9
anderson.sanji <eusanji@hotmail.com>
2026-01-21 14:23:42 +0100

    Add model package and Car

---
+++ src/main/java/com/springweb/model/Car.java
@@ -0,0 +1,38 @@
+package com.springweb.model;
+
+public class Car {
+	
+	private int carId;
+    private String manufacturer;
+    private int model;
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getModel() {
+        return model;
+    }
+
+    public void setModel(int model) {
+        this.model = model;
+    }
+    
+    @Override
+    public String toString() {
+        return "Car[carId=" + carId +", manufacturer='" + manufacturer + '\'' + ", model=" + model + ']';
+    }
+
+}
