2075f1258414574b558d9418527270a171aa42cb
anderson.sanji <eusanji@hotmail.com>
2026-01-21 14:49:50 +0100

    Adding database connection package and class DbUtil

--- src/main/java/com/springweb/util/DbUtil.java
+++ src/main/java/com/springweb/util/DbUtil.java
@@ -1,5 +1,43 @@
 package com.springweb.util;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DbUtil {
+	
+	private static Connection connection = null;
+	
+	public static Connection getConnection(){
+        if (connection != null){
+            return connection;
+        } else {
+            try {
+                Properties properties = new Properties();
+                InputStream inputStream = DbUtil.class.getClassLoader().getResourceAsStream("/db.properties");
+                properties.load(inputStream);
+                String driver = properties.getProperty("driver");
+                String url = properties.getProperty("url");
+                String user = properties.getProperty("user");
+                String password = properties.getProperty("password");
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url,user,password);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        }
+    }
+	
 
 }
