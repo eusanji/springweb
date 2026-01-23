b5f0790e823c7569be5a8869bbc90852ba4530a4
anderson.sanji <eusanji@hotmail.com>
2026-01-24 00:32:04 +0100

    Add doGet for getting the car and editing

---
+++ src/main/java/com/springweb/controller/CarController.java
@@ -0,0 +1,60 @@
+package com.springweb.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import com.springweb.dao.CarDao;
+import com.springweb.model.Car;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class CarController extends HttpServlet {
+	 private static final long serialVersionUID = 1L;
+	    private static String INSERT_OR_EDIT = "/car.jsp";
+	    private static String LIST_USER = "/listCar.jsp";
+	    private CarDao dao;
+	    ApplicationContext context;
+
+	    public CarController() {
+	        super();
+	        context = new ClassPathXmlApplicationContext("spring.xml");
+	        dao = (CarDao) context.getBean("CarDao");
+	    }
+
+	    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	        String forward = "";
+	        String action = request.getParameter("action");
+	        try {
+	            if(action.equalsIgnoreCase("delete")){
+	                int CardId = Integer.parseInt(request.getParameter("cardId"));
+	                dao.deleteCar(CardId);
+	                forward = LIST_USER;
+	                request.setAttribute("cars", dao.getAllCars());
+	            } else if (action.equalsIgnoreCase("edit")) {
+	                forward = INSERT_OR_EDIT;
+	                int CardId = Integer.parseInt(request.getParameter("cardId"));
+	                Car car = dao.getCarById(CardId);
+	                request.setAttribute("car", car);
+	            } else if (action.equalsIgnoreCase("listCar")){
+	                forward = LIST_USER;
+	                request.setAttribute("cars", dao.getAllCars() );
+	            } else {
+	                forward = INSERT_OR_EDIT;
+	            }
+	            RequestDispatcher view = request.getRequestDispatcher(forward);
+	            view.forward(request,response);
+	        } catch (SQLException e){
+	            e.printStackTrace();
+	        }
+
+
+	    }
+}
